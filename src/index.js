65c37962a61c8765a9df907c1c88576a53a59347
RebecaGSP <38191659+RebecaGSP@users.noreply.github.com>
2020-12-26 19:13:08 -0600

    hours and minutes less than 10 fixed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@
 function formatDate(timestamp) {
   let date = new Date(timestamp);
   let hour = date.getHours();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
   let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
   let weekDay = [
     "Sunday",
     "Monday",
